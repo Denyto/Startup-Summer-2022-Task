5c25174c5d39f900b4b392734062ecb70468951a
Denyto <denyto@tut.by>
2022-05-14 00:35:52 +0300

    init: create react app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>DENYTO</p>
+        <p>DENYTO DENYTO</p>
       </header>
     </div>
   );
